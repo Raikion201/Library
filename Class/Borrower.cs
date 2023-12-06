9917ce24d91bd7d16cf99c5e91073c240212dccc
myuh250 <89782377+myuh250@users.noreply.github.com>
2023-12-07 04:24:28 +0700

    add borrow and return book feature

--- Class/Borrower.cs
+++ Class/Borrower.cs
@@ -2,25 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
 
 namespace Library
 {
-    public class Borrower
+    public class Borrower : User
     {
-        public int BorrowerId { get; set; }
-        public string Name { get; set; }
-        public List<Book> BooksBorrowed { get; set; }
-
-        public Borrower(int borrowerId, string name, List<Book> booksBorrowed)
+        public string ContactInformation { get; set; }
+        public string BorrowerID { get; set; }
+		public List<HistoryLog> HistoryLog { get; private set; }
+        public Borrower(string name, string contactInformation, string borrowerId)
         {
-            BorrowerId = borrowerId;
             Name = name;
-            BooksBorrowed = new List<Book>();
+            ContactInformation = contactInformation;
+            BorrowerID = borrowerId;
+			HistoryLog = new List<HistoryLog>();
         }
 
-        public bool IsBorrowed(Book book)
+        // Read all borrower from CSV file
+        // example read data: John Doe johndoe@gmail.com 75
+        public static List<Borrower> ReadBorrowersFromCSV(string filePath) {
+            List<Borrower> borrowers = new List<Borrower>();
+
+            try
+			{
+				using (TextFieldParser parser = new TextFieldParser(filePath))
+				{
+					parser.TextFieldType = FieldType.Delimited;
+					parser.SetDelimiters(",");
+
+					while (!parser.EndOfData)
+					{
+                        string[] data = parser.ReadFields()!;
+
+                        Borrower borrower = new Borrower(data[0], data[1], data[2]);
+
+                        borrowers.Add(borrower);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occurred: {ex.Message}");
+			}
+
+            return borrowers;
+        }
+
+        // Borrow book
+        public void BorrowBook(Borrower borrower, Book book) 
         {
-            return BooksBorrowed.Any(borrowedBook => borrowedBook.ID == book.ID); // Assuming ID is a unique identifier for books
+            if (book.Availability == true && book.Quantity > 0) 
+            {
+                book.Quantity--;
+                if (book.Quantity == 0) book.Availability = false;
+                book.BorrowStatus = true;
+                book.Borrowers.Add(this);
+
+                // Add history log, borrow date is today, return date is 1 days from today
+                HistoryLog.Add(new HistoryLog(this, book, DateTime.Now, DateTime.Now.AddDays(1)));
+            }
+        }
+
+        // Return book
+        public void ReturnBook(Borrower borrower, Book book) 
+        {
+            // Find the log record to return
+            HistoryLog recordToReturn = HistoryLog.FirstOrDefault(record => record.Book == book && record.Borrower == borrower);
+            
+            if (recordToReturn != null) 
+            {
+                book.Quantity++;
+                book.Availability = book.Quantity > 0;
+                book.BorrowStatus = false;
+                // Remove the borrower from the book
+                book.Borrowers.Remove(borrower);
+            }
+            // Remove the log record from the borrower
+            // If we don't remove it, the borrower will have a list of log records that are already returned
+            HistoryLog.Remove(recordToReturn);
         }
     }
 }
